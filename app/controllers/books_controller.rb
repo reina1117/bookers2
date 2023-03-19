d06bfb3fb01a9a551ee1176847683bb91a2fa841
reina1117 <122454145+reina1117@users.noreply.github.com>
2023-03-19 04:50:30 +0000

    [Finish] bookのindexページの左側

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,23 @@
 class BooksController < ApplicationController
   def index
+    @user = current_user
     @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to root_path
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about'
+  post 'books' => 'books#create'
   devise_for :users
   
   resources :users, only: [:index, :show, :edit]
-  resources :books, only: [:index, :show, :edit]
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
