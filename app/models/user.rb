52cc554a1d60c1b826ee0e50c9b4aa68312c18a8
reina1117 <122454145+reina1117@users.noreply.github.com>
2023-03-19 11:38:13 +0000

    [Finish] バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :post_images, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
 end
